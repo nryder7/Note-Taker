9d18c1771d860839d0ba2c4e6d79f90097f4d498
Nicholas Ryder <nryder7@gmail.com>
2020-09-02 21:15:48 -0400

    delete button works

--- Develop/server.js
+++ Develop/server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
 //var util = require("util");
-const dataBase = require("./db/db.json")
+var dataBase = require("./db/db.json")
 const PORT = 3001;
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -21,24 +23,58 @@ app.get("/api/notes", function (req, res) {
 app.post("/api/notes", function (req, res) {
 
     var newNote = req.body;
-
-    console.log (newNote);
+    // newNote.id = JSON.stringify(dataBase.uuidv4());
+    newNote.id = JSON.stringify(Math.random());
     dataBase.push(newNote);
 
-    fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(dataBase), function(err) {
-     if(err) throw err;  
-     res.json([dataBase])
-     });
-   
+    fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(dataBase), function (err) {
+        if (err) throw err;
+        res.json([dataBase])
+    });
+
 });
 
-// app.post("/api/notes", function (req, res) {
-//     req.
-// }
+app.delete("/api/notes/:id", function (req, res) {
+    fs.readFile("./db/db.json", "utf8", function(err, data) {
+    if (err) {
+        throw err;
+    }
+    else {
+        dataBase = JSON.parse([data])
+        dataBase = dataBase.filter(function (dataBase) {
+            return dataBase.id !== req.params.id;
+        });
+
+        fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify([...dataBase]), "utf8", function (err) {
+            if (err) throw err;
+            res.json([dataBase])
+        });
+        
+    }
+});
+});
 
 
-// app.get("/notes", function (req, res) {
-//     res.sendFile(path.join(__dirname, "./public/notes.html"));
+
+// fs.readFile("./db/db.json", "utf8", function(err, data) {
+//      if(err) {
+//          throw err;
+//      }
+//      else {
+//          dataBase = JSON.parse([data])
+//          console.log(dataBase);
+//          console.log("------------");
+//          dataBase = dataBase.filter(function(dataBase) {
+//             return dataBase.id !== '3';
+
+//         });
+//         console.log(dataBase);
+//         fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(dataBase), function(err) {
+//             if(err) throw err;  
+//             // res.json([dataBase])
+//             });
+
+//      }
 // });
 
 
