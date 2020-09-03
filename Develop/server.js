de71e8cca65d2f35f20c7b4d9b737ee67daa09b4
Nicholas Ryder <nryder7@gmail.com>
2020-09-02 21:52:48 -0400

    update delete function, randomID, change port for heroku, add get for *

--- Develop/server.js
+++ Develop/server.js
@@ -2,11 +2,10 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const fs = require("fs");
-const { v4: uuidv4 } = require('uuid');
+//const { v4: uuidv4 } = require('uuid');
 //var util = require("util");
 var dataBase = require("./db/db.json")
-const PORT = 3001;
-
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -35,48 +34,20 @@ app.post("/api/notes", function (req, res) {
 });
 
 app.delete("/api/notes/:id", function (req, res) {
-    fs.readFile("./db/db.json", "utf8", function(err, data) {
-    if (err) {
-        throw err;
-    }
-    else {
-        dataBase = JSON.parse([data])
-        dataBase = dataBase.filter(function (dataBase) {
-            return dataBase.id !== req.params.id;
-        });
 
-        fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify([...dataBase]), "utf8", function (err) {
+    dataBase = dataBase.filter(function (dataBase) {
+                return dataBase.id !== req.params.id;
+            });
+
+        fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(dataBase), function (err) {
             if (err) throw err;
-            res.json([dataBase])
-        });
-        
-    }
-});
-});
+            res.json(dataBase)
+        });    
+    })
 
-
-
-// fs.readFile("./db/db.json", "utf8", function(err, data) {
-//      if(err) {
-//          throw err;
-//      }
-//      else {
-//          dataBase = JSON.parse([data])
-//          console.log(dataBase);
-//          console.log("------------");
-//          dataBase = dataBase.filter(function(dataBase) {
-//             return dataBase.id !== '3';
-
-//         });
-//         console.log(dataBase);
-//         fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(dataBase), function(err) {
-//             if(err) throw err;  
-//             // res.json([dataBase])
-//             });
-
-//      }
-// });
-
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "./public/index.html"));
+    });
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
